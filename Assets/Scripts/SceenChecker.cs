0e6c37631e0775c3452b4a9a9086e6dda498e930
Tindavetran10 <102245594+Tindavetran10@users.noreply.github.com>
2024-10-29 14:01:37 +0700

    Fix NullReference of Camera

--- Assets/Scripts/SceenChecker.cs
+++ Assets/Scripts/SceenChecker.cs
@@ -42,10 +42,12 @@ public class ScreenChecker : MonoBehaviour
     // Hàm để kiểm tra vị trí của chim so với nửa chiều cao màn hình
     public bool IsBirdBelowHalfScreen(Vector3 birdPosition)
     {
+        if (_mainCamera == null) return false;
         // Lấy toạ độ y của điểm nửa chiều cao màn hình
         float screenHalfY = _mainCamera.transform.position.y - _screenHalfHeight;
 
         // Kiểm tra nếu vị trí y của chim nhỏ hơn nửa chiều cao màn hình
         return birdPosition.y < screenHalfY;
+
     }
 }
